c410c43166936c6fa93029cc65da44f0b7ae169e
Hyeon-hak Kim <07ily@naver.com>
2023-08-19 14:35:39 +0900

    delete deprecated projects

--- react-projects/hello-react/src/App.js
+++
@@ -1,7 +0,0 @@
-import MyComponent from "./ToDoTemplate/MyComponent";
-
-const App = () => {
-  return <MyComponent></MyComponent>;
-};
-
-export default App;
--- react-projects/hello-react/webpack.config.js
+++
@@ -1,5 +0,0 @@
-// webpack.config.js
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const dotenv = require("dotenv").config();
