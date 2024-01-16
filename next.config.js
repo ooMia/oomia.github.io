9899ab6532476fe6c0d2ea9846c072faab48e4be
Hyeon-hak Kim <07ily@naver.com>
2024-01-16 11:21:41 +0900

    🚚 Chore : set config for static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports#configuration
+  output: 'export',
 }
 
 module.exports = nextConfig
