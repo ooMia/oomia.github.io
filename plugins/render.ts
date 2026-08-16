716848d408900614f15573de5f081a4e72735031
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2026-08-17 05:45:21 +0900

    fix: use static imports

--- plugins/render.ts
+++ plugins/render.ts
@@ -1,8 +1,8 @@
 import type { DataEntry, RenderResult } from "./types";
+import { render as astroRender } from "astro:content";
 import schema from "./schema";
 
 export default async function render(entry: DataEntry): Promise<RenderResult> {
-  const { render: astroRender } = await import("astro:content");
   const result = await astroRender(entry);
   const remarkPluginFrontmatter = schema.parse(result.remarkPluginFrontmatter);
   return {
