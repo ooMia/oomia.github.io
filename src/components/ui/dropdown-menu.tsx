b007ccf694f3208d6a7d7eb3f0cc5d5cd2f0a45c
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2026-08-20 08:42:26 +0900

    feat: theme switcher

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -108,7 +108,7 @@ function DropdownMenuItem({
       data-slot="dropdown-menu-item"
       data-inset={inset}
       data-variant={variant}
-      textValue={typeof children === "string" ? children : props.textValue}
+      textValue={typeof children === "string" ? children : props.textValue!}
       className={composeRenderProps(className, (className, { selectionMode }) =>
         cn(dropdownMenuItemVariants({ selectionMode }), className),
       )}
@@ -151,7 +151,7 @@ function DropdownMenuSubTrigger({
     <MenuItemPrimitive
       data-slot="dropdown-menu-sub-trigger"
       data-inset={inset}
-      textValue={typeof children === "string" ? children : props.textValue}
+      textValue={typeof children === "string" ? children : props.textValue!}
       className={cn(
         "flex cursor-default items-center gap-2 rounded-xl px-3 py-2 text-sm outline-hidden select-none focus:bg-accent focus:text-accent-foreground not-data-[variant=destructive]:focus:**:text-accent-foreground data-inset:ps-9.5 data-open:bg-accent data-open:text-accent-foreground [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
